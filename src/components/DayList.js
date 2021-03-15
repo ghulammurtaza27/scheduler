5d129b8846e6f1bb8711d05058d41e581efde414
Ghulam Murtaza <Ghulam.murtaza@mail.mcgill.ca>
2021-03-16 04:08:43 +0500

    day list test and components implemented

---
+++ src/components/DayList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import DayListItem from "./DayListItem";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
+export default function DayList(props){
+
+  const day = days.map(day => {
+    return (
+      <ul>
+        <DayListItem
+          key = {day.id}
+          name={day.name} 
+          spots={day.spots} 
+          selected={day.name === props.day}
+          setDay={props.setDay}  
+        />
+      </ul>
+    );
+  })
+  return day;
+};
